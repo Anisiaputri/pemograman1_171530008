3bea1f463e7c7a59d86c773fca50c00f165b8f28
Anisiaputri <36910868+Anisiaputri@users.noreply.github.com>
2018-04-03 12:12:57 +0700

    Add files via upload

---
+++ Praktikum 6 - BOOSTSTRAP/home.php
@@ -0,0 +1,4 @@
+<div class="halaman">
+    <h2>Halaman depan</h2>
+    <p>selamat datang. ini merupakan halaman tentang demo cara membuat halaman website tanpa reload dengan jquery</p>
+</div>
---
+++ Praktikum 6 - BOOSTSTRAP/tutorial.php
@@ -0,0 +1,4 @@
+<div class="tutorial">
+    <h2>tutorial</h2>
+    <p>selamat datang. ini merupakan halaman tentang demo cara membuat halaman website tanpa reload dengan jquery</p>
+</div>
